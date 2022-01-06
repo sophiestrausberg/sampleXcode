89bc935b9a8379fed04bc9637d3f60c3fe34542f
Sophie Strausberg <sophie.strausberg@gmail.com>
2022-01-06 15:28:29 -0500

    Initial Commit

---
+++ gyyufftu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gyyufftu
+//
+//  Created by Sophie Strausberg on 1/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gyyufftu/gyyufftuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gyyufftuApp.swift
+//  gyyufftu
+//
+//  Created by Sophie Strausberg on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct gyyufftuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

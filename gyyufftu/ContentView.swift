64bc6a7a2892f47355c6c8f2b540a90b760c35d0
Sophie Strausberg <sophie.strausberg@gmail.com>
2022-01-06 16:44:40 -0500

    add "hey.swift"

--- gyyufftu/ContentView.swift
+++ gyyufftu/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, fyufyfyuffutuft!")
+        Text("Hello there!")
             .padding()
     }
 }
--- gyyufftu/hey.swift
+++ gyyufftu/hey.swift
@@ -1,13 +1,8 @@
 //
-//  file.swift
+//  hey.swift
 //  gyyufftu
 //
 //  Created by Sophie Strausberg on 1/6/22.
 //
 
 import Foundation
-
-class Working {
-    var work = true
-    var day = "every"
-}
